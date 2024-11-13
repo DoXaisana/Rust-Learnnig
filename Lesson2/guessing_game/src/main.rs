05ccdcc7533a04dc98968c2687e7788bf89df78d
unknown <dodoxaisana123@gmail.com>
2024-11-13 09:12:05 +0700

    finish lesson2

--- Lesson2/guessing_game/src/main.rs
+++ Lesson2/guessing_game/src/main.rs
@@ -1,6 +1,6 @@
-use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
@@ -9,19 +9,29 @@ fn main() {
 
     println!("Your secret number is {secret_number}");
 
-    println!("Please input for guess.");
+    loop {
+        println!("Please input for guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read the line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read the line");
 
-    println!("You guess: {guess}");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!!"),
-        Ordering::Greater => println!("Too big!!"),
-        Ordering::Equal => println!("You win!!!!"),
+        println!("You guess: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!!"),
+            Ordering::Greater => println!("Too big!!"),
+            Ordering::Equal => {
+                println!("You win!!!!");
+                break;
+            },
+        }
     }
 }
